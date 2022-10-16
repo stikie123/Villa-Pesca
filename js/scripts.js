eb62f9815f51e0c8015f590e22610f21611f98ad
stikie123 <stianbekker@gmail.com>
2022-10-16 17:53:54 +0100

    1.2.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+// from price per room per night
+const pricePerNight = 1000
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -51,3 +54,108 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 // Google map
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+   document.getElementById("nextBtn").innerHTML = "Book Now"
+   document.getElementById("nextBtn").setAttribute("type", "Submit")
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+// lets customer know what they will pay per night
+let dateS = 0;
+let dateE = 0;
+let difference = 0;
+let amountRooms = 0;
+document.getElementById("amountRooms").addEventListener("change", function(){
+  let amount = this.value
+  return amountRooms = amount
+});
+document.getElementById("start").addEventListener("change", function(){
+  let date = this.value;
+  let date1 = new Date(date);
+  return dateS = date1.getTime();
+});
+document.getElementById("end").addEventListener("change", function(){
+  let date = this.value;
+  let date1 = new Date(date);
+  return dateE = date1.getTime();
+  
+});
+function calc(){ if (dateE && dateS > 0) {
+  difference = (dateE - dateS)/(1000*60*60*24);
+  document.getElementById("fromPPN").innerHTML = difference*pricePerNight*amountRooms;
+} }
+function changeRooms(){
+  document.getElementById("start").value = "yyyy/mm/dd"
+  document.getElementById("end").value = "yyyy/mm/dd"
+  document.getElementById("fromPPN").innerHTML = 0
+  return dateE = 0 , dateS = 0 , difference = 0
+};
+
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
